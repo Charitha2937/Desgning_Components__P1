9dbba256804323d10c25528ca10cbbda5f862c25
Charitha2937 <charitharayaluthirumalasetty@gmail.com>
2025-03-20 16:17:39 +0530

    Completed Assignment

--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,1 +1,21 @@
 // write the component code here
+import React from 'react';
+import '../index.css'; 
+
+const UserCard = () => {
+
+  return (
+    <div className="user-card">
+      <img src='../assets/@techabbayi.png' alt="Profile" className="photo" />
+      <div className="details">
+        <h1>#Kalvian UserCard</h1>
+        <h2 className="name">Charitha Rayalu</h2>
+        <p className="email">Email: charitharayalu.thirumalasetty.s86@kalvium.community</p>
+        <p className="phone">Phone: +91 1234567890</p>
+        <p className="address">Address:The Apollo University, Chittoor, Andhra Pradesh, India </p>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
